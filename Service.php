070c977a82d405c9092b9d8d6f6598676ba1745d
josias <antsiasnirina@gmail.com>
2017-05-23 18:13:57 +0300

    create a new server machine

--- Service.php
+++ Service.php
@@ -18,4 +18,32 @@ class Service implements \Box\InjectionAwareInterface
 	{
 		return $this->di ;
 	}
+
+	public function install()
+	{
+		$sql = "
+        CREATE TABLE IF NOT EXISTS `craftsrv_machine` (
+            `id` bigint(20) NOT NULL AUTO_INCREMENT,
+            `host` varchar(255) NOT NULL,
+            `version` varchar(2) NOT NULL,
+            `token` varchar(255) NOT NULL,
+            PRIMARY KEY (`id`),
+            ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;";
+        $this->di['db']->exec($sql);
+	}
+
+	private function createServerMachine(array $srvMachine_array)
+	{
+        $srvMachine = $this->di['db']->dispense('craftsrv_machine') ;
+        foreach ($srvMachine_array as $field => $value) {
+        	$srvMachine->$field = $value ;
+        }
+        $this->di['db']->store($srvMachine);
+        return $srvMachine;
+	}
+
+	public function uninstall()
+    {
+        $this->di['db']->exec("DROP TABLE IF EXISTS `craftsrv_machine`");
+    }
 }
